bd1ab43b2560e6db3052d903e78f57031ff244a9
macorains <mac.rainshrine@gmail.com>
2019-11-01 22:02:17 +0900

    フォーム編集、フォーム項目編集、転送リストのヘルプを追加

---
+++ src/components/help/form/FormColEdit.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="help-sub2">
+    <div class="container">
+      <h3>
+        フォーム項目編集画面
+      </h3>
+      <img
+        src="sample.png"
+        width="800"
+        height="600"
+      >
+      <dl>
+        <dt>項目名</dt>
+        <dd>画面に表示するフォーム項目名</dd>
+        <dt>項目ID</dt>
+        <dd>内部でフォーム項目を識別するためのID（画面には表示されない）</dd>
+        <dt>型</dt>
+        <dd>フォーム項目の型を指定</dd>
+        <dt>選択項目</dt>
+        <dd>型がラジオボタン、チェックボックス、コンボボックスの場合に、選択項目を設定 「追加」ボタンクリックで項目を追加できる</dd>
+        <dt>初期値</dt>
+        <dd>フォーム項目の初期値を設定</dd>
+        <dt>バリデーション</dt>
+        <dd>フォーム入力内容のバリデーション（入力値検査）を設定</dd>
+        <dt>数値範囲</dt>
+        <dd>バリデーションが「数値のみ」の場合に、入力を許容する数値範囲を設定</dd>
+        <dt>文字数範囲</dt>
+        <dd>入力を許容する文字数の範囲を設定</dd>
+        <dt>必須項目</dt>
+        <dd>このフォーム項目を入力必須項目とする</dd>
+      </dl>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'FormColEdit'
+}
+</script>
--- src/components/help/transfer/Transfer.vue
+++ src/components/help/transfer/Transfer.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="help-sub">
     <div class="container">
-      <h2>
-        転送
-      </h2>
+      <h3>
+        転送タスク
+      </h3>
+      <img src="sample.png" width="800" height="600">
+      <dl>
+        <dt>No.</dt>
+        <dd>設定したフォームのデータ受信時に実行するタスクの実行順序番号</dd>
+        <dt>転送名</dt>
+        <dd>転送タスク名</dd>
+        <dt>転送タイプ</dt>
+        <dd>
+          転送の種別
+          <ul>
+            <li>Salesforce: SalesforceのAPIを通じてSalesforceにフォームから送られてきたデータを登録</li>
+            <li>Mail: フォームからデータが送られてきた時にメールを送信</li>
+          </ul>
+        </dd>
+        <dt>操作</dt>
+        <dd>
+          <ul>
+            <li>編集</li>
+            <li>削除</li>
+          </ul>
+        </dd>
+      </dl>
     </div>
   </div>
 </template>
