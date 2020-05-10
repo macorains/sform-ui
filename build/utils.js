0df3c92fd43fae541494e6cc6f432255a3bdae74
macorains <mac.rainshrine@gmail.com>
2020-05-10 14:05:03 +0900

    修正もれFix

--- build/utils.js
+++ build/utils.js
@@ -1,6 +1,5 @@
 var path = require('path')
 var config = require('../config')
-var ExtractTextPlugin = require('mini-css-extract-plugin')
 
 exports.assetsPath = function (_path) {
   var assetsSubDirectory = process.env.NODE_ENV === 'production'
@@ -35,10 +34,7 @@ exports.cssLoaders = function (options) {
     // Extract CSS when that option is specified
     // (which is the case during production build)
     if (options.extract) {
-      return ExtractTextPlugin.extract({
-        use: loaders,
-        fallback: 'vue-style-loader'
-      })
+      return ['vue-style-loader', MiniCssExtractPlugin.loader].concat(loaders)
     } else {
       return ['vue-style-loader'].concat(loaders)
     }
