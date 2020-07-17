081497030d0074d36ee830ba14586e0f9a98298c
macorains <mac.rainshrine@gmail.com>
2020-07-17 22:26:50 +0900

    SFORM-29 転送設定のリスト表示部分実装

--- src/components/admin/transfers/TransferConfig.vue
+++ src/components/admin/transfers/TransferConfig.vue
@@ -1,6 +1,70 @@
 <template>
   <div class="transferConfig">
     <div class="container">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">
+              No.
+            </th>
+            <th scope="col">
+              {{ $t('message.transfer_config_name') }}
+            </th>
+            <th scope="col">
+              {{ $t('message.status') }}
+            </th>
+            <th scope="col">
+              {{ $t('message.transfer_type') }}
+            </th>
+            <th scope="col">
+              {{ $t('message.action') }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="item in transferConfigList"
+            :key="item.config_index"
+          >
+            <th scope="row">
+              {{ Number(item.config_index) }}
+            </th>
+            <td>
+              {{ item.name }}
+            </td>
+            <td>
+              {{ item.status }}
+            </td>
+            <td>
+              {{ item.type_code }}
+            </td>
+            <td>
+              <b-btn
+                size="sm"
+                @click="test(item.col_index)"
+              >
+                <span
+                  class="oi oi-pencil"
+                  title="pencil"
+                  aria-hidden="true"
+                />
+                {{ $t('message.edit') }}
+              </b-btn>
+              <b-btn
+                size="sm"
+                @click="test(item.col_index)"
+              >
+                <span
+                  class="oi oi-trash"
+                  title="trash"
+                  aria-hidden="true"
+                />
+                {{ $t('message.delete') }}
+              </b-btn>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -11,6 +75,11 @@ export default {
     serverUri: {
       type: String,
       default: ''
+    }
+  },
+  data: function () {
+    return {
+      transferConfigList: []
     }
   },
   created: function () {
@@ -25,8 +94,14 @@ export default {
     }
     this.$http.get(this.$props.serverUri + '/transfer/config/list', this.$data.config)
       .then(response => {
-        console.log(response)
+        this.$data.transferConfigList = response.data
+        console.log(response.data)
       })
+  },
+  methods: {
+    test: function () {
+      alert('!')
+    }
   }
 }
 </script>
