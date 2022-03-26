a2e9d7dda14721e91a5a2403f6f1bd289ead7dd1
macorains <mac.rainshrine@gmail.com>
2022-03-26 17:00:02 +0900

    SFORM-70 debug

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -72,7 +72,8 @@ export default {
     // TODO この辺りは後々環境変数から取るように変更する
     const clientId = '485408982983-42gd7gfheac6vbfs8seb7nlsrfibcvma.apps.googleusercontent.com'
     const scope = 'https://www.googleapis.com/auth/cloud-platform'
-    const redirectUri = 'https://admin.it.sform.app/api/oauthToken'
+    // const redirectUri = 'https://admin.it.sform.app/api/oauthToken'
+    const redirectUri = 'http://localhost:9001/oauthToken'
     const requestUri = `https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}`
     // TODO OAuth tokenがLocalstorageに無い時だけ呼ぶように変更する
     location.href = requestUri
