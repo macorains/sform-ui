6d0a5310603b321eb6105e510172c4d2489de4fb
macorains <mac.rainshrine@gmail.com>
2023-04-06 00:35:04 +0900

    Test token取るときにJSON.parseしてみる

--- src/components/Signin.vue
+++ src/components/Signin.vue
@@ -77,9 +77,9 @@ export default {
       params.append('username', this.email)
       params.append('group', this.group)
       params.append('password', this.password)
-      const token = localStorage.getItem('sformToken')
-      console.log(token)
-      this.$http.defaults.headers.common['X-Auth-Token'] = token
+      // const token = localStorage.getItem('sformToken')
+      // console.log(token)
+      // this.$http.defaults.headers.common['X-Auth-Token'] = token
 
       this.$http.post('/signIn', params)
         .then(response => {
