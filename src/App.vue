431f79b3ab6ad3ffb6c74dd0c9c35ebd37d1ba72
macorains <mac.rainshrine@gmail.com>
2023-04-18 01:00:09 +0900

    Test 送り方を変える

--- src/App.vue
+++ src/App.vue
@@ -136,7 +136,7 @@ export default {
       if (newToken) {
         // IDtoken作成済みなら検証
         // TODO play側でJWT検証ロジック書いて呼び出すようにする
-        this.$http.get('/iapVerify/' + newToken).then(response => {
+        this.$http.get('/iapVerify/' + newToken, { headers: { Authorization: 'Bearer ' + newToken } }).then(response => {
           console.log('***** jwt *****')
           console.log(response)
         })
