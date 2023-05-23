a882abdda79eed476425b1ec72a939f70c1de055
macorains <mac.rainshrine@gmail.com>
2023-05-23 23:09:43 +0900

    Test 'x-Requested-With': 'XMLHttpRequest'

--- src/axios/index.js
+++ src/axios/index.js
@@ -4,7 +4,7 @@ const http = Axios.create({
   baseURL: process.env.VUE_APP_API_URL,
   withCredentials: true,
   headers: {
-    'x-Requested-With': '*',
+    'x-Requested-With': 'XMLHttpRequest',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
     'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
