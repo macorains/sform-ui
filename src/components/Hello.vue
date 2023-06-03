759f00c4c31825a49dfd1c8331795b58a3897335
macorains <mac.rainshrine@gmail.com>
2023-06-03 22:12:58 +0900

    SFORM-88 jwt取得をApp.vue側に移す

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -19,12 +19,6 @@ export default {
     }
   },
   created: function () {
-    const token = localStorage.getItem('sformToken')
-    if (!token) {
-      location.href = process.env.VUE_APP_API_URL + '/jwt'
-    }
-    this.$http.defaults.headers.common['X-Auth-Token'] = token
-
     this.$http.get('/adminExistsCheck')
       .then(response => {
         if (response.data.result === false) {
