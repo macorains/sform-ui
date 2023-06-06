6b1628dce40bfc578fe478ef8963f5a42fdde742
macorains <mac.rainshrine@gmail.com>
2023-06-06 23:39:27 +0900

    SFORM-89 チェック終わるまでrouter-view非表示にしてみる

--- src/App.vue
+++ src/App.vue
@@ -107,6 +107,7 @@
       </b-collapse>
     </b-navbar>
     <router-view
+      v-if="loaded"
       :hashed-form-id="hashedFormId"
       @updateHashedFormId="updateHashedFormId"
       @updateIsAdmin="updateIsAdmin"
@@ -119,6 +120,7 @@ export default {
   name: 'App',
   data: function () {
     return {
+      loaded: false,
       hashedFormId: '',
       isAdmin: false,
       axiosTimeout: 3000,
@@ -129,10 +131,11 @@ export default {
   created: function () {
     const token = localStorage.getItem('sformToken')
     if (!token) {
+      // TODO この時画面切り替わりに時間かかるケースがあるので、何とかする
       location.href = process.env.VUE_APP_API_URL + '/jwt'
     }
     this.$http.defaults.headers.common['X-Auth-Token'] = token
-
+    this.$data.loaded = true
     window.addEventListener('error', event => {
       if (this.$route.path === '/signin') {
         this.$data.isAdmin = false
